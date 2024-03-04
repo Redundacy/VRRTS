5a55912d8074f90c5730303a2b4608367191405e
Eric Dundas <kuribodragon@gmail.com>
2024-03-04 17:04:00 -0600

    laser pointing

--- Assets/UnitMove.cs
+++ Assets/UnitMove.cs
@@ -9,15 +9,37 @@ using Valve.VR;
 /// </summary>
 public class UnitMove : MonoBehaviour
 {
+    public SteamVR_Action_Boolean isPointing = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
+    public GameObject pointingHand;
+    public LayerMask collectedLayers;
+    public float raycastDistance;
+
+    private LineRenderer lrend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lrend = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPointing.state)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(pointingHand.transform.position, pointingHand.transform.position + pointingHand.transform.forward, out hit, raycastDistance)) //currently pointing in the wrong direction (and also I would like to change the angle
+            {
+                lrend.SetPosition(0, pointingHand.transform.position);
+                lrend.SetPosition(1, hit.point);
+            } else
+            {
+                lrend.SetPosition(0, pointingHand.transform.position);
+                lrend.SetPosition(1, pointingHand.transform.position + pointingHand.transform.forward);
+            }
+            Debug.Log("pointing? " + hit.collider);
+        } else if (isPointing.stateUp)
+        {
+            Debug.Log("not pointing anymore");
+        }
     }
 }
