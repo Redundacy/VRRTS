a1a34395faebc4155236f4353dcd298bb2ed6367
Eric Dundas <kuribodragon@gmail.com>
2024-03-04 15:47:49 -0600

    split nav testing into two duplicates for new testing

--- Assets/UnitMove.cs
+++ Assets/UnitMove.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.AI.Navigation;
+using Valve.VR;
 
 /// <summary>
 /// This component helps player raycast components tell selected units where to move
